3ef4f44fa34671e50418806bdf1c699e0c8dfc92
Bruno Rodrigues <brunocroh@gmail.com>
2017-05-22 00:44:56 -0300

    git init

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,30 @@
+class NegociacaoController{
+
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+
+    adiciona(event) {
+        event.preventDefault();
+
+        let negociacao = new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+        this.limpaCampos();
+        console.log(negociacao);
+    }
+
+    limpaCampos(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
+    }
+
+}
